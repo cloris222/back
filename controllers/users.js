6d052f92191f699740e53d0ecc40573f77c9cb53
cloris222 <cloris222@gmail.com>
2023-02-06 09:38:44 +0800

    update

--- controllers/users.js
+++ controllers/users.js
@@ -45,3 +45,44 @@ export const login = async (req, res) => {
     res.status(500).json({ success: false, message: '未知錯誤' })
   }
 }
+
+export const logout = async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter(token => token !== req.token)
+    await req.user.save()
+    res.status(200).json({ success: true, message: '' })
+  } catch (error) {
+    res.status(500).json({ success: false, message: '未知錯誤' })
+  }
+}
+
+export const extend = async (req, res) => {
+  try {
+    const idx = req.user.tokens.findIndex(token => token === req.token)
+    const token = jwt.sign({ _id: req.user._id }, process.env.JWT_SECRET, { expiresIn: '7 days' })
+    req.user.tokens[idx] = token
+    await req.user.save()
+    res.status(200).json({ success: true, message: '', result: token })
+  } catch (error) {
+    res.status(500).json({ success: false, message: '未知錯誤' })
+  }
+}
+
+export const getUser = (req, res) => {
+  try {
+    res.status(200).json({
+      success: true,
+      message: '',
+      result: {
+        account: req.user.loginaccount,
+        email: req.user.loginemail,
+        cart: req.user.cart,
+        favorites: req.user.loginfavorites,
+        role: req.user.loginrole
+      }
+    })
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ success: false, message: '未知錯誤' })
+  }
+}
