8e6c854f95d4eb87d1d76e9d64d6ac620ce9adbb
cloris222 <cloris222@gmail.com>
2023-02-07 09:14:51 +0800

    update

--- controllers/users.js
+++ controllers/users.js
@@ -4,18 +4,18 @@ import jwt from 'jsonwebtoken'
 export const register = async (req, res) => {
   try {
     await users.create({
-      account: req.body.registeraccount,
-      password: req.body.registerpassword,
-      name: req.body.registername,
-      email: req.body.registeremail,
-      phone: req.body.registerphone
+      account: req.body.account,
+      password: req.body.password,
+      name: req.body.name,
+      email: req.body.email,
+      phone: req.body.phone
     })
     res.status(200).json({ success: true, message: '' })
   } catch (error) {
-    console.log(error.errors)
     if (error.name === 'validationError') {
       res.status(400).json({ success: false, message: error.errors[Object.keys(error.errors)[0]] })
     } else if (error.name === 'MongoServerError' && error.code === 11000) {
+      console.log(error)
       res.status(400).json({ success: false, message: '帳號重複' })
     } else {
       console.log(error)
